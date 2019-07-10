26bc91847b5b34b6f8e4482c2cbcd14a65bb69fe
ShobiPr <shobiha.premkumar@stud.ntnu.no>
2019-07-10 14:21:18 +0200

    Created Point class and started on CheckPosition

---
+++ java/telia/cpa/location/Point.java
@@ -0,0 +1,13 @@
+package telia.cpa.location;
+
+public class Point {
+
+        long x;
+        long y;
+
+        public Point(int x, int y){
+            this.x = x;
+            this.y = x;
+        }
+
+}
