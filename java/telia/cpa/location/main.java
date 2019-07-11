72c2dcdd5bad485ba7a051e07c31dd6e7ca59ae4
Mvrcus97 <marcus-tierney@hotmail.com>
2019-07-11 09:52:41 +0200

    GetPolygonAsPoints

--- java/telia/cpa/location/main.java
+++ java/telia/cpa/location/main.java
@@ -2,7 +2,7 @@ package telia.cpa.location;
 
 public class main {
     public static void main(String[] args) {
-        no.telia.cpa.location.CoverageAreaInvoker client = new no.telia.cpa.location.CoverageAreaInvoker();
+        CoverageAreaInvoker client = new CoverageAreaInvoker();
 
 
         client.setMsisdn("4793859166");
@@ -10,16 +10,21 @@ public class main {
         System.out.println("Country: " + client.getCountry() + ", County: " + client.getCounty() + ", Council: " + client.getCouncil());
         System.out.println();
 
-        client.setMsisdn("4798437400");
-        System.out.println("Location of number: " + client.getMsisdn() + " is:  " + client.getLocation());
-        System.out.println("Country: " + client.getCountry() + ", County: " + client.getCounty() + ", Council: " + client.getCouncil() + ", SubPart Area: " + client.getSubPartArea());
-        System.out.println("Coverage Area: " + client.getMultiPolygon());
-        System.out.println("Location time: "  + client.getLocationTime());
-
+        client.setMsisdn("4792291073");
         System.out.println();
         System.out.println(client.toString());
 
-        System.out.println("KOMMMMMENTAR");
+        System.out.println(client.getMultiPolygon());
+
+        Point[] points = client.getPolygonAsPoints();
+        System.out.println("List of points generated: ");
+        for(int i = 0; i < points.length; i++){
+            System.out.println("(" + points[i].x +", " + points[i].y + ")");
+        }
+
+
+        System.out.println();
+
     }
 
 }
