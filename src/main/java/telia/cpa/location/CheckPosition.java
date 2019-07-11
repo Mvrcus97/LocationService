e903407a2484a7cc8383fb1af7aee1ab2b9b7bb1
ShobiPr <shobiha.premkumar@stud.ntnu.no>
2019-07-11 09:42:40 +0200

    TDD

--- src/main/java/telia/cpa/location/CheckPosition.java
+++ src/main/java/telia/cpa/location/CheckPosition.java
@@ -3,17 +3,17 @@ package telia.cpa.location;
 public class CheckPosition {
 
     // check if q lies on segment p-r
-    public boolean onSegment(Point p, Point q, Point r){
+    public boolean onSegment(Point p, Point q, Point r) {
 
-        if (q.x <= Math.max(p.x, r.x) && q.x >= Math.min(p.x,r.x) &&
-            q.y <= Math.max(p.y, r.y) && q.y >= Math.min(p.y,r.y)){
+        if (q.x <= Math.max(p.x, r.x) && q.x >= Math.min(p.x, r.x) &&
+                q.y <= Math.max(p.y, r.y) && q.y >= Math.min(p.y, r.y)) {
             return true;
         }
-            return false;
+        return false;
     }
 
     // 0 - colinear, 1 - clockwise, 2 - counterclockwise
-    public int orientation(Point p, Point q, Point r){
+    public int orientation(Point p, Point q, Point r) {
         double val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
 
         if (val == 0) return 0;
@@ -23,7 +23,7 @@ public class CheckPosition {
 
 
     // true if line segment (p1q1) and (p2q2) intersect
-    public boolean doIntersect(Point p1, Point q1, Point p2, Point q2){
+    public boolean doIntersect(Point p1, Point q1, Point p2, Point q2) {
 
         int o1 = orientation(p1, q1, p2);
         int o2 = orientation(p1, q1, q2);
@@ -31,7 +31,7 @@ public class CheckPosition {
         int o4 = orientation(p2, q1, p1);
 
         //general case
-        if (o1 != o2 && o3 != o4) return  true;
+        if (o1 != o2 && o3 != o4) return true;
 
         if (o1 == 0 && onSegment(p1, p2, q1)) return true;
 
@@ -45,7 +45,11 @@ public class CheckPosition {
     }
 
     // n vertices
-    boolean isInside(Point polygon[], Point p){
+    boolean isInside(Point polygon[], Point p) {
+
+        if (polygon == null || p == null){
+            return false;
+        }
 
         int n = polygon.length;
 
@@ -62,11 +66,11 @@ public class CheckPosition {
             int next = (i + 1) % n;
 
             // check if p-extreme intersects with polygon[i]-polygon[next]
-            if (doIntersect(polygon[i], polygon[next], p, extreme)){
+            if (doIntersect(polygon[i], polygon[next], p, extreme)) {
 
                 // if point p is colinear with i-next, then check if it lies on i-next
                 // if yes -> return true, otherwise false
-                if (orientation(polygon[i], p, polygon[next]) == 0){
+                if (orientation(polygon[i], p, polygon[next]) == 0) {
                     return onSegment(polygon[i], p, polygon[next]);
                 }
 
