d68e15dc3a779a044b4f3e669fa3ec22e674c52d
Mvrcus97 <marcus-tierney@hotmail.com>
2019-07-30 14:27:12 +0200

    FIxed for MarcusComputerFilePath

--- src/main/java/telia/cpa/location/quiz/UpdatePositionJob.java
+++ src/main/java/telia/cpa/location/quiz/UpdatePositionJob.java
@@ -114,28 +114,20 @@ public class UpdatePositionJob implements Job {
 
         user.updateLevel();
 
-        System.out.println("LEVEL: " + user.getLevel());
-        System.out.println("LOCATOIN SIZE: " + quizLocations.size());
-
-        System.out.println("NEXT HINT: " + quizLocations.get(user.getLevel()).getHint());
-
-        System.out.println(user.getMsisdn() + " Level up! - Now on level " + user.getLevel() + " 🏋️‍ ");
+        System.out.println(user.getMsisdn() + " Level up! - Now on level " + user.getLevel() + " 🏋️‍ ");
         logger.info(user.getMsisdn() + " Level up! -  "+ user.getLevel());
 
-        String text = "Congratulations!\n" +
-                       "New level: " + user.getLevel() + "\n" +
-                       "Your score is: " + user.getScore() + "\n" +
-                        promo.getPromoText() + "\n\n" +
-                       "Find the next secrete location: " + quizLocations.get(user.getLevel()).getHint();
+        String text = "Congratulations, " + user.getFirstName() + "!\n\n" +
+                "New level: " + user.getLevel() + "\n" +
+                "Your score is: " + user.getScore() + "\n\n" +
+                promo.getPromoText() + "\n\n" +
+                "Find the next secrete location: " + quizLocations.get(user.getLevel() -1 ).getHint();
 
         client.addMessage(user.getMsisdn(),text);
-
-        System.out.println("MESSAGE SENT");
 
         if (user.getLevel() >= quizLocations.size()) {
             user.resetLevel();
         }
-
 
     }
 
