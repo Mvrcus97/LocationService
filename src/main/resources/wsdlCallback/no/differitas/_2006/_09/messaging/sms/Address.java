08430f7bdb8a839035b5d57edd6e1c786889c58e
ShobiPr <shobiha.premkumar@stud.ntnu.no>
2019-07-31 15:36:33 +0200

    Added Callback support

---
+++ src/main/resources/wsdlCallback/no/differitas/_2006/_09/messaging/sms/Address.java
@@ -0,0 +1,87 @@
+
+package no.differitas._2006._09.messaging.sms;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlValue;
+
+
+/**
+ * <p>Java class for Address complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="Address">
+ *   &lt;simpleContent>
+ *     &lt;extension base="&lt;http://www.w3.org/2001/XMLSchema>string">
+ *       &lt;attribute name="format" type="{http://differitas.no/2006/09/messaging/sms}AddressFormat" />
+ *     &lt;/extension>
+ *   &lt;/simpleContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "Address", propOrder = {
+    "value"
+})
+public class Address {
+
+    @XmlValue
+    protected String value;
+    @XmlAttribute(name = "format")
+    protected AddressFormat format;
+
+    /**
+     * Gets the value of the value property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getValue() {
+        return value;
+    }
+
+    /**
+     * Sets the value of the value property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    /**
+     * Gets the value of the format property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link AddressFormat }
+     *     
+     */
+    public AddressFormat getFormat() {
+        return format;
+    }
+
+    /**
+     * Sets the value of the format property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link AddressFormat }
+     *     
+     */
+    public void setFormat(AddressFormat value) {
+        this.format = value;
+    }
+
+}
---
+++ src/main/resources/wsdlCallback/no/differitas/_2006/_09/messaging/sms/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://differitas.no/2006/09/messaging/sms", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package no.differitas._2006._09.messaging.sms;
